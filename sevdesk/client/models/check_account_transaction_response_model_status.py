3f125e2c5048d8dafbd0b47460e4e5ba6ac4c835
Jörg Thalheim <joerg@thalheim.io>
2023-07-08 11:39:29 +0200

    add undocumented transaction status code

--- sevdesk/client/models/check_account_transaction_response_model_status.py
+++ sevdesk/client/models/check_account_transaction_response_model_status.py
@@ -5,7 +5,8 @@ class CheckAccountTransactionResponseModelStatus(str, Enum):
     VALUE_0 = "100"
     VALUE_1 = "200"
     VALUE_2 = "300"
-    VALUE_3 = "400"
+    VALUE_3 = "350"
+    VALUE_4 = "400"
 
     # The SevDesk API might use "0" for null-enums
     NULL = "0"
